4dbeb60cc60009c21dad41b2d6db7cd936aea6ea
William Whitted <youngedre@gmail.com>
2019-10-05 14:09:38 -0400

    Port environment variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser")
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
